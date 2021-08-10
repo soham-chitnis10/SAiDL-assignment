963538fa423b08274d286ed58847279f5a5a0d13
soham-chitnis10 <77532586+soham-chitnis10@users.noreply.github.com>
2021-08-10 13:54:41 +0530

    updated reward function

--- RL/Quadratic_2D_env.py
+++ RL/Quadratic_2D_env.py
@@ -6,7 +6,6 @@ import random
 import time
 
 
-
 class QuadraticEnv(Env):
     def __init__(self):
         self.seed()
@@ -39,13 +38,13 @@ class QuadraticEnv(Env):
         return [seed]
     def step(self,action):
         done = False
-        reward = self.reward_new(action)
+        reward = self.reward_updated(action)
         self.state[0]-= action[0][0]
         self.state[1]-= action[0][1]
         self.steps += 1
         if abs(self.state[0]-self.x_min)<0.1 and abs(self.state[1]-self.y_min)<0.1 :
             done = True
-        elif self.steps >= 200:
+        elif self.steps > 200:
             done = True
 
         return self.state,reward,done
@@ -63,15 +62,42 @@ class QuadraticEnv(Env):
         self.steps = 0
         return self.state
     def reward(self):
+        '''
+        Reward function:
+        Depends only on the state 
+        Reward is given by th inverse of the distance between the state and the goal.
+        '''
         state = self.state
         dist = np.sqrt((state[0]-self.x_min)**2 + (state[1]-self.y_min)**2)
         reward = 1/dist
         return reward
     def reward_new(self,action):
+        '''
+        Reward function:
+        Depends on state as well as action
+        Reward is given by exponentiation of the difference in coordinates with action along both directions.
+        Reward is restricted to be less than 1e+17
+        '''
         reward = np.exp(action[0][0]*(-(self.x_min - self.state[0]))) + np.exp(action[0][1]*(-(self.y_min - self.state[1])))
         while reward > 1e+17:
           reward /= 1e+17
         return reward
+    def reward_updated(self,action):
+      '''
+      Reward function:
+      Depends on state as well as action
+      Reward is awarded as 0 when action is the opposite direction e.g x coordinate is supposed to increased to reach the minima but the action is to reduce the x cooordinate in that case reward is 0.
+      '''
+      if (self.state[0]-self.x_min)*action[0][0] > 0:
+        r_x = (self.state[0]-self.x_min)*action[0][0]
+      else:
+        r_x = 0
+      if (self.state[1]-self.y_min)*action[0][1] > 0:
+        r_y = (self.state[1]-self.y_min)*action[0][1]
+      else:
+        r_y = 0
+      reward = r_x + r_y
+      return reward
     def start_time(self):
         self.start = time.time()
     def end_time(self):

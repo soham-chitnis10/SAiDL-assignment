de4dc59b3ff6501c0c7b1c743e20234169b2d5b4
soham-chitnis10 <77532586+soham-chitnis10@users.noreply.github.com>
2021-08-10 13:57:28 +0530

    added testing

--- RL/main.py
+++ RL/main.py
@@ -32,7 +32,7 @@ def main():
 
     
 
-    # start of experiment: Keep looping until desired amount of episodes reached
+    # start of training: Keep looping until desired amount of episodes reached
     max_episodes = 1000
     total_episodes = 0 # keep track of amount of episodes that we have done
     max_reward = 0
@@ -68,6 +68,43 @@ def main():
     plt.show()
     print('Successful Episodes:')
     print(successful_episodes)
+
+    print('Testing starts:')
+    #start of testing
+    max_episodes = 1000
+    total_episodes = 0 # keep track of amount of episodes that we have done
+    max_reward = 0
+    max_reward_ep = 0
+    reward_list = []
+    succesful_episodes = []
+    while total_episodes < max_episodes:
+
+        obs = env.reset()
+        done = False
+        episode_reward = 0 # keep track of rewards per episode
+
+        while not done:
+            action, ln_prob = policy.select_action(np.array(obs))
+            next_state, reward, done = env.step(action)
+            trajectory.append([obs, action, ln_prob, reward, next_state, done])
+            obs = next_state
+            episode_reward += reward
+        reward_list.append(env.reward())
+        print(f'Episode: {total_episodes} Reward:{episode_reward} function: {env.a}x^2 + {env.b}y^2 + {env.c}xy + {env.d}x + {env.e}y + {env.f} x:{env.state[0]} y:{env.state[1]} x_min:{env.x_min} y_min:{env.y_min}')
+        if abs(env.state[0]-env.x_min) < 0.1 and (env.state[1]-env.y_min) < 0.1:
+          succesful_episodes.append(total_episodes)
+        if episode_reward > max_reward:
+            max_reward = episode_reward
+            max_reward_ep = total_episodes
+
+        total_episodes += 1
+    
+    print(f'Max Reward is {max_reward} occured on episode {max_reward_ep}')
+    eps = [ep for ep in range(1,max_episodes+1)]
+    plt.plot(eps,reward_list)
+    plt.show()
+    print('Succesful Episodes:')
+    print(succesful_episodes)
 
 
 
